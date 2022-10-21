c6d9958c951be8d3870136636182f170a1934769
Cybernetic1 <generic.intelligence@gmail.com>
2022-10-21 15:40:14 +0800

    Project Graph UI works now

--- python/test-visdcc.py
+++ python/test-visdcc.py
@@ -1,8 +1,10 @@
 # TO-DO:
-# * link nodes
 
 # DONE:
 # * radio buttons for Done (pink) vs In-progress
+# * link nodes
+# * color node bug (fixed: set global variable)
+# * fill task_name when select node
 
 import dash
 import visdcc
@@ -25,8 +27,10 @@ styles = {
 
 app.layout = html.Div([
 	visdcc.Network(id = 'net',
-		options = dict(height= '600px', width= '600px'),
-		style = { 'nodes': {
+		options = dict(height= '600px', width= '100%'),
+		style = {
+		'background-color': '#C9F9FF',
+		'nodes': {
 		'font': {
 			# required: enables displaying <b>text</b> in the label as bold text
 			'multi': 'html',
@@ -53,15 +57,14 @@ app.layout = html.Div([
 	html.Button('Delete node', id='delNode'),
 	html.Button('Link nodes', id='linkNodes'),
 	html.Button('Renew node details', id='renewNode'),
-	html.Br(),html.Br(),
+	html.P(children = "Selected Nodes: ②=none ①=none", id='selectedNode'),
+	html.Br(),
 	html.Button('Save graph', id='saveGraph'),
 	html.Button('Load graph', id='loadGraph'),
-	html.Br(),html.Br(),
-	html.Pre("Selected Node =", id='selectedNode'),
 ])
 
-selected_node_1 = "none"
-selected_node_2 = "none"
+selected_node_1 = None
+selected_node_2 = None
 
 task_name = ""
 task_status = None
@@ -103,32 +106,45 @@ for n in nodes[1:]:
 
 # ===== Process callback events =====
 
-@app.callback(
+@app.callback(								# select Node in graph
 	Output('selectedNode', 'children'),
 	Input('net', 'selection'))
-def clicked_node(x):
+def clicked_node(selected):
 	global selected_node_1, selected_node_2
-	if x is None:
-		return "none"
-	elif len(x['nodes']) > 0:
-		selected_node_1 = x['nodes'][0]
-		return str(selected_node_1)
-	else:
-		return "none"
+	print("selected =", selected)
+	if selected is not None and len(selected['nodes']) > 0:
+		newNode = selected['nodes'][0]
+		if selected_node_1 != newNode:
+			selected_node_2 = selected_node_1
+			selected_node_1 = newNode
+	name_1 = next((n['label'] for n in net['nodes'] if n["id"] == selected_node_1), "none")
+	name_2 = next((n['label'] for n in net['nodes'] if n["id"] == selected_node_2), "none")
+	return "Selected Nodes: ②=[" + name_2 + "] ①=[" + name_1 + "]"
 
-@app.callback(
+@app.callback(								# Task Name changed by clicking node
+	Output(component_id='task_name', component_property='value'),
+	Input('net', 'selection'))
+def myfun(selected):
+	if selected is not None and len(selected['nodes']) > 0:
+		s = selected['nodes'][0]
+		val = next((n['label'] for n in net['nodes'] if n["id"] == s), None)
+		return val
+	return None
+
+@app.callback(								# typing in Task Name
 	Output(component_id='task_nameLabel', component_property='children'),
-	Input(component_id='task_name', component_property='value')
+	Input(component_id='task_name', component_property='value'),
 	)
 def myfun(val):
 	global task_name
 	task_name = val
-	return "Task name = " + val
+	return "Task name = " + (val if val is not None else "")
 
 @app.callback(								# Task status = Done / inProgress
     Output('taskStatus', 'style'),
     Input('Status', 'value'))
 def myfun(val):
+	global task_status
 	print("Status =", val)
 	task_status = val
 	if val == 'pink':
@@ -136,7 +152,7 @@ def myfun(val):
 	else:
 		return {'color':'blue'}
 
-@app.callback(
+@app.callback(								# Click various buttons
 	Output('net', 'data'),
 	dash.dependencies.Input('addNode', 'n_clicks'),
 	dash.dependencies.Input('delNode', 'n_clicks'),
@@ -146,12 +162,12 @@ def myfun(val):
 	dash.dependencies.Input('loadGraph', 'n_clicks')
 	)
 def myfun(btn1, btn2, btn3, btn4, btn5, btn6):
-	global net, node_index, task_name, selected_node_1, selected_node_2
+	global net, node_index, task_name, task_status, selected_node_1, selected_node_2
 	print("node_index =", node_index)
 	print("task_name =", task_name)
 	print("selected_node_1 =", selected_node_1)
 	# print("triggered = ", callback_context.triggered)
-	button_id = callback_context.triggered[0]['prop_id'] if not None else 'No clicks yet'
+	button_id = callback_context.triggered[0]['prop_id']
 	print(button_id + " clicked")
 
 	if button_id[:7] == 'addNode':
@@ -167,6 +183,16 @@ def myfun(btn1, btn2, btn3, btn4, btn5, btn6):
 			print("matching:", n)
 			if n['id'] == selected_node_1:
 				net['nodes'].remove(n)
+		# Don't forget to delete all edges to OR from this node!
+		collection = []
+		for e in net['edges']:
+			print("matching:", e)
+			if e['to'] == selected_node_1 or \
+			   e['from'] == selected_node_1:
+				print("Will delete edge:", e)
+				collection.append(e)
+		for e in collection:
+			net['edges'].remove(e)
 		# print("result =", net)
 		return net
 
@@ -185,8 +211,8 @@ def myfun(btn1, btn2, btn3, btn4, btn5, btn6):
 		return net
 
 	if button_id[:9] == 'saveGraph':
-		json_str = json.dumps(net, indent=2)
-		# json_str = json.dumps(net, separators=(',', ':'))
+		# json_str = json.dumps(net, indent=2)
+		json_str = json.dumps(net, separators=(',', ':'))
 		print("To save: ", json_str)
 		with open("ProjectGraph.json", "w+") as outfile:
 			outfile.write(json_str)
