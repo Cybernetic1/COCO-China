4dc5aa3faf2f5ee069dd153dfd4122643b9f806e
Cybernetic1 <generic.intelligence@gmail.com>
2022-10-24 22:55:33 +0800

    non-integer vote slider

--- js/vote-slider.js
+++ js/vote-slider.js
@@ -72,3 +72,74 @@ for (var i = 0; i < max_i; i++) {
 		}
 	}
 
+// =============== Non-integer version ===================
+
+var sliders2 = document.getElementsByClassName("slider2");
+var outputs2 = document.getElementsByClassName("score2");
+
+var scores2 = [];
+var max_j = 6;
+
+// **** Initialize sliders values
+for (var j = 0; sliders[j]; j++) {
+	scores2[j] = 1000.0 / max_j;
+	sliders2[j].value = Math.round(scores2[j]);
+	outputs2[j].innerHTML = (scores2[j] / 10.0).toFixed(2);
+}
+
+var total2 = 0.0;
+for (var k = 0; k < max_j; k++)
+	total2 += scores2[k];
+console.log("total2 =", total2 / 10.0);
+document.getElementById("total2").innerHTML = (total2 / 10.0).toFixed(2);
+
+// **** Update the current slider value (each time you drag the slider handle)
+for (var j = 0; j < max_j; j++) {
+	// **** For each slider:
+	sliders2[j].oninput = function() {
+		var k = parseInt(this.id)				// get slider number
+		scores2[k] = parseFloat(this.value);
+		outputs2[k].innerHTML = parseFloat(scores2[k] / 10.0);
+
+		// 计算盈馀
+		var subtotal = 0;
+		for (var i = 0; i < max_j; i++)
+			subtotal += scores2[i];
+		// console.log("subtotal = " + subtotal.toString());
+		var surplus = 1000.0 - subtotal;
+		// console.log("surplus = " + surplus.toString());
+		var adjustment = surplus / (max_j - 1);
+		// console.log("adjustment = " + adjustment.toString());
+
+		// **** 将盈馀分配给各会员
+		var remainder = surplus;				// This will eventually be the rounding error
+		const h = max_j - 1;
+		const b = subtotal - scores2[k];
+		for (var i = 0; i < max_j; i++)
+			if (i != k) {
+				if (b < 1e-5)
+					adjustment = (1000.0 - scores2[k]) / h;
+				else
+					// The below formula is correct because ∑_k scores[k] / subtotal = 1
+					adjustment = surplus * scores2[i] / b;
+				remainder -= adjustment;
+				scores2[i] += adjustment;
+				}
+		// console.log("remainder = ", remainder);
+
+		// update 之
+		for (var i = 0; i < max_j; i++) {
+			if (i != k) {
+				sliders2[i].value = Math.round(scores2[i]);
+				outputs2[i].innerHTML = parseFloat(scores2[i] / 10.0).toFixed(2);
+				}
+			}
+
+		// **** Display finalized total
+		var total = 0.0;
+		for (var i = 0; i < max_j; i++)
+			total += scores2[i];
+		document.getElementById("total2").innerHTML = parseFloat(total / 10.0).toFixed(2);
+		}
+	}
+
